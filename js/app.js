a96fbdd6593cda673a8fcc09a89cd7a9edfbd690
Antonio <aruballo@uci.edu>
2015-06-24 22:02:41 -0700

    Display first 100 prices for search term

--- js/app.js
+++ js/app.js
@@ -14,9 +14,50 @@ ebayTrends.searchView = Backbone.View.extend({
         var parameters = 'SECURITY-APPNAME=AntonioR-c20d-4f92-aad4-791bfb005d8c&' +
             'OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&RESPONSE-DATA-FORMAT=JSON' +
             '&REST-PAYLOAD&keywords=' + searchValue;
-        ebayTrends.currentCollection = new ebayTrends.collection();
-        ebayTrends.currentCollection.fetch({dataType: 'jsonp', data: parameters});
+        this.currentCollection = new ebayTrends.collection();
+        this.currentCollection.fetch({dataType: 'jsonp', data: parameters, success: (function(context){
+            return function(){
+                context.createTable();
+            };
+        })(this)});
+      
+    },
+    
+    createTable: function(){
+        var currentResults = new ebayTrends.tableView({
+            collection: this.currentCollection
+        });
+        var $table = currentResults.render().$el;
+        this.$el.append($table);
     }
+});
+
+ebayTrends.tableRowView = Backbone.View.extend({
+    template: _.template($('#resultsRow').html()),
+    render: function(eventName){
+        var html = this.template(this.model.toJSON());
+        this.setElement($(html));
+        return this;
+    }
+});
+
+ebayTrends.tableView = Backbone.View.extend({
+    tagName: 'table',
+    initialize: function(){
+        this.collection.bind("reset", this.render, this);
+    },
+    render: function(eventName){
+        this.$el.empty();
+        
+        this.collection.each(function(result){
+            var row = new ebayTrends.tableRowView({model: result});
+            var $tr = row.render().$el;
+            this.$el.append($tr);
+        }, this);
+        
+        return this;
+    }
+    
 });
 
 
--- js/model.js
+++ js/model.js
@@ -6,6 +6,10 @@ ebayTrends.collection = Backbone.Collection.extend({
     
     model: ebayTrends.model,
     
-    url: 'http://svcs.ebay.com/services/search/FindingService/v1'
+    url: 'http://svcs.ebay.com/services/search/FindingService/v1',
+    
+    parse: function(data){
+        return data.findItemsByKeywordsResponse[0].searchResult[0].item;
+    }
     
 });
